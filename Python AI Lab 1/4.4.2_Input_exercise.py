a8d5a2dbc083e41735dcfb824695e62b8ed05fde
Saky AIU <114678581+sakyAIU@users.noreply.github.com>
2023-03-19 19:47:52 +0800

    Python AI Lab 1
    Basic Python

---
+++ Python AI Lab 1/4.4.2_Input_exercise.py
@@ -0,0 +1,21 @@
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+result = int(num1) + int(num2)
+print("The sum is- ", result)
+
+result = int(num1) - int(num2)
+print("The sub is-", result)
+
+# What we can see here, it's boring to declare variable type again and again. To get rid of these, we  can declare type with input-
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+result = num1 + num2
+print("The sum is: ", result)
+
+result = num1 - num2
+print("The sub is: ", result)
+
